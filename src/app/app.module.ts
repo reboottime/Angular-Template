8a8fa36dedca76fdf4fe7e56f1885fe62e3e679d
KateZ <hello.katezhang@gmail.com>
2023-12-07 17:54:53 -0500

    Bootstrap application using app.module

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    CommonModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
