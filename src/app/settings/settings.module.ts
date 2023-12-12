e7102824ec6fee35bc022e5d4b9094afb1ad7622
KateZ <hello.katezhang@gmail.com>
2023-12-12 13:24:19 -0500

    Add reactive form example

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 
@@ -8,6 +9,8 @@ import { SettingsRoutingModule } from './settings-routing.module';
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     SettingsRoutingModule
   ]
 })
